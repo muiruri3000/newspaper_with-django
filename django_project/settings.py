529cdc2fb0a92cd21e18363e252e80e8aadc09eb
muiruri3000 <josee.muiruri95@gmail.com>
2026-01-02 10:19:59 +0300

    allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,6 +15,7 @@ from environs import Env
 
 env = Env()
 env.read_env()
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
