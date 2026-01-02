fa7e32cdc8d196f34aad43e10ce10859c3aadab0
muiruri3000 <josee.muiruri95@gmail.com>
2026-01-02 15:35:51 +0300

    updated article create url

--- articles/urls.py
+++ articles/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
     path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
-    path("new/", ArticleCreateView.as_view(), name="new"),
+    path("new/", ArticleCreateView.as_view(), name="article_new"),
 ]
